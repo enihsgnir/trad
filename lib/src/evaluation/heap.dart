89cc55d0ee78699022ee37c3f42adf9ea1fda50e
enihsgnir <enihsgnir@gmail.com>
2026-06-14 18:36:21 +0900

    refactor: centralize instance ID allocation in Heap

--- lib/src/evaluation/heap.dart
+++ lib/src/evaluation/heap.dart
@@ -1,15 +1,19 @@
 import 'instance.dart';
 
-extension type InstanceId(int id) {
-  InstanceId operator +(int value) => InstanceId(id + value);
-}
+extension type InstanceId(int value) {}
 
 class Heap {
   final Map<InstanceId, Instance> _instances = {};
   InstanceId _nextInstanceId = InstanceId(0);
 
+  InstanceId _allocateInstanceId() {
+    final id = _nextInstanceId;
+    _nextInstanceId = InstanceId(id.value + 1);
+    return id;
+  }
+
   InstanceId allocate(Instance instance) {
-    final id = _nextInstanceId++;
+    final id = _allocateInstanceId();
     _instances[id] = instance;
     return id;
   }
